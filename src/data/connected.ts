071f12ce6487544a0d3b285803ff854cbd9a6ad5
Mann Chhikara <75443469+Mann003@users.noreply.github.com>
2023-11-02 23:56:51 +0530

    First Commit

---
+++ src/data/connected.ts
@@ -0,0 +1,31 @@
+const connected = [
+  {
+    id: 1,
+    name: "Vishnu Swaroop",
+    role: "Full Stack Developer",
+    company: "Emp",
+  },
+  {
+    id: 2,
+    name: "Vishnu Swaroop",
+    role: "Full Stack Developer",
+    company: "Emp",
+  },
+];
+
+const suggestions = [
+  {
+    id: 3,
+    name: "Vishnu Swaroop",
+    role: "Full Stack Developer",
+    company: "Emp",
+  },
+  {
+    id: 4,
+    name: "Vishnu Swaroop",
+    role: "Full Stack Developer",
+    company: "Emp",
+  },
+];
+
+export { connected, suggestions };
